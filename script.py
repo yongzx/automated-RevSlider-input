916d22a9b046d94f07160fd48fa67f41dbb8fb85
yongzx <zhengxin.yong@minerva.kgi.edu>
2017-07-01 20:45:09 +0800

    Documentation

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import openpyxl
-import copy  # for deep copy
+import copy
 import re
 import os
 
@@ -10,9 +10,12 @@ TITLE, ID, START_TIME, END_TIME = range(4)
 class ExcelData:
     """
     ExcelData contains all the data in a Excel sheet
+    
+    Arguments:
+    None
 
     Container: list
-    To access individual Topic data, ExcelData_instance[row_number-2][column_number][category]
+    To access individual Topic data, ExcelData_instance[row_number_from_excel-2][column_number][category]
 
     Functions:
     read(TopicData object, Excel) : add all the information in the Excel into m_list. TopicData stores the information in a particular row.
@@ -76,6 +79,10 @@ class ExcelData:
 class TopicData:
     """
     TopicData object contains the information in a particular row
+    
+    Arguments:
+    None
+
     Container: dictionary
     key --> column number
     value --> list(   TITLE, (ID, (START_TIME, END_TIME))   ) 
@@ -194,8 +201,28 @@ class TopicData:
 
 class Output:
     """
-    read template files.
-    replace the information in the template file with the data from the ExcelData.
+    Read template files and replace the information in the template file with the data from the ExcelData.
+    
+    Arguments:
+    header template
+    body template
+    footer template
+
+    Functions:
+    modify_HeadSegment() : change title in header
+    modify_HeadSlidesN() : change total number of slides in header
+    modify_Body() : check the amount of information of a cell --> control flow for modify_Body1() and modify_Body2()
+    modify_Body1() : modify the body template in the cell and put it in a list to be concatenated later (for cell information == 4)
+    modify_Body2() : modify the body template in the cell and put it in a list to be concatenated later (for cell information > 4)
+    joinBody(): call modify_Body()
+    modify_SystemId(): Read in a unique ID and modify the body template
+    empty(): resets the container holding the modified body templates, index, and slide order
+    makedir(): create a folder
+    cddir(): enter a folder
+    exitdir(): exit from a folder
+    export(): export multiple .txt files, which are ready to be imported into Wordpress database, into a newly created folder  
+
+    
     """
 
     def __init__(self, head_file, body_file, end_file):
@@ -215,20 +242,22 @@ class Output:
         self.INDEX = 0
 
     def modify_HeadSegment(self, row):
-        # segment number
-        #row_number = range(1,len(video1))
+        """
+        modify the title for the to-be-imported file.
+        """
         string_to_replace = r's:5:"title";s:2:"i1";s:5:"alias";s:2:"i1";s:9:"shortcode";s:23:"\[rev_slider alias="i1"]"'
         replacement_in_tuple = ('s:5:"title";s:2:"i', str(row), '";s:5:"alias";s:2:"i', str(row),
                                 '";s:9:"shortcode";s:23:"[rev_slider alias="i', str(row), '"]"')
 
         replacement_string = ''.join(replacement_in_tuple)
-        # print(replacement_string)
         changed = re.sub(string_to_replace,
                          replacement_string, self.head_text0)
         self.head_text = changed
-        # print(self.head_text)
 
     def modify_HeadSlidesN(self):
+        """
+        modify the total number of slides
+        """
         string_to_replace = r's:17:"custom_javascript";s:0:"";}s:6:"slides";a:7:{'
         replacement_in_tuple = (
             's:17:"custom_javascript";s:0:"";}s:6:"slides";a:', str(self.INDEX), ':{')
@@ -237,18 +266,40 @@ class Output:
         self.head_text = changed
 
     def modify_Body(self, row, body_text, video_class):
+        """
+        check the amount of information in a particular cell
+
+        Arguments:
+        row --- particular row in an excel sheet. accessed from Exceldata object.
+        body_text --- body template .txt file
+        video_class --- Exceldata object
+        """
         for key in range(1, len(video_class[row]) + 1):
-            if len(video_class[row][key]) == 4:
-                self.modify_Body1(row, key, body_text, video_class)
-            elif len(video_class[row][key]) > 4:
-                self.modify_Body2(row, key, body_text, video_class)
+            if len(video_class[row][key]) == 4: 
+                self.modify_Body1(row, key, body_text, video_class) #create a slide
+            elif len(video_class[row][key]) > 4: 
+                self.modify_Body2(row, key, body_text, video_class) #create multiple slides
 
     def modify_Body1(self, row, key, txtfile, video_class):
+        """
+        modify the body template in the cell and put it in a list to be concatenated later.
+        (for cell information == 4)
+        
+        Arguments: 
+        row --- particular row in an excel sheet. accessed from Exceldata object.
+        key --- key-value pair, in which the key is paired with the information in a particular cell.  
+        txtfile --- body template .txt file
+        video_class --- Exceldata object
+
+        """
+
+        #index
         index_to_replace = r'xi:0'
         index_replacement_in_tuple = ('i:', str(self.INDEX))
         index_replacement_string = ''.join(index_replacement_in_tuple)
         self.INDEX += 1
 
+        #slide order
         slide_order_to_replace = r's:11:"slide_order";s:1:"1"'
         slide_order_replacement_in_tuple = ('s:11:"slide_order";s:', str(
             len(str(self.SLIDE_ORDER))), ':"', str(self.SLIDE_ORDER), '"')
@@ -256,30 +307,36 @@ class Output:
             slide_order_replacement_in_tuple)
         self.SLIDE_ORDER += 1
 
+        #title of the video
         title_to_replace = r's:15:"Bozeman Science"'
         title_replacement_in_tuple = ('s:', str(
             len(video_class[row][key][TITLE])), ':"', video_class[row][key][TITLE], '"')
         title_replacement_string = ''.join(title_replacement_in_tuple)
 
+        #size of ID
         size_id_to_replace = r's:11:"wxvERNlUdBQ"'
         size_id_replacement_in_tuple = ('s:', str(
             len(video_class[row][key][ID])), ':"', video_class[row][key][ID], '"')
         size_id_replacement_string = ''.join(size_id_replacement_in_tuple)
 
+        #ID
         id_to_replace = r'wxvERNlUdBQ'
         id_replacement_string = video_class[row][key][ID]
 
+        #starting time
         start_time_to_replace = r's:4:"4:30"'
         start_time_replacement_in_tuple = ('s:', str(len(video_class[row][key][
                                            START_TIME])), ':"', video_class[row][key][START_TIME], '"')
         start_time_replacement_string = ''.join(
             start_time_replacement_in_tuple)
 
+        #ending time 
         end_time_to_replace = r's:4:"9:06"'
         end_time_replacement_in_tuple = ('s:', str(
             len(video_class[row][key][END_TIME])), ':"', video_class[row][key][END_TIME], '"')
         end_time_replacement_string = ''.join(end_time_replacement_in_tuple)
 
+        #modifying process
         sub_index = re.sub(index_to_replace, index_replacement_string, txtfile)
         sub_slide_order = re.sub(
             slide_order_to_replace, slide_order_replacement_string, sub_index)
@@ -296,6 +353,17 @@ class Output:
         self.list_body_text.append(sub_end_time)
 
     def modify_Body2(self, row, key, txtfile, video_class):
+        """
+        modify the body template in the cell and put it in a list to be concatenated later
+        (for cell information > 4)
+
+        Arguments: 
+        row --- particular row in an excel sheet. accessed from Exceldata object.
+        key --- key-value pair, in which the key is paired with the information in a particular cell.  
+        txtfile --- body template .txt file
+        video_class --- Exceldata object
+        """
+
         loop_index = 1  # negate the name
         title_index = 1
         total_title_index = 0
@@ -385,10 +453,23 @@ class Output:
     # combine all the bodies
     # each body represents individual cell
     def joinBody(self, row, video_class):
+        """call the modify_Body"""
         self.modify_Body(row, self.body_text0, video_class)
         return self.list_body_text
 
     def modify_SystemId(self, txtfile):
+        """
+        Read in a unique ID. 
+        Since currently wordpress database automatically updates the ID of the imported file, this member function
+            does not update the system ID in the txtfile from which it reads in the system ID. 
+        
+        Arguments:
+        txtfile --- contains the ID number
+
+        Returns:
+        None
+
+        """
         base_system_id_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Templates", txtfile), 'r+')
         base_system_id = int(base_system_id_file.read())
         list_i = 0
@@ -404,24 +485,43 @@ class Output:
             list_i += 1
 
     def empty(self):
+        """resets:
+        list_body_test --- which holds all the information to replace the body template file
+        slide_order --- starts from 1
+        index --- starts from 0
+        """
         self.list_body_text = []
         self.SLIDE_ORDER = 1
         self.INDEX = 0
 
     def makedir(self, file_name):
+        """create a folder"""
         try:
             os.makedirs(file_name)
         except OSError:
             pass
 
     def cddir(self, file_name):
+        """enter the folder"""
         os.chdir(file_name)
 
     def exitdir(self):
+        """exit from the folder to the main folder automated_RevSlider_input"""
         os.chdir(os.path.dirname(os.path.dirname( os.path.abspath(__file__))))
 
     # export the txt which is read to be imported
     def export(self, excel_name, sheet_name, video_class):
+        """
+        Create a folder and save multiple the txt files which are ready to be exported into the folder
+        
+        Arguments:
+        excel_name --- name of the excel file
+        sheet_name --- name of the sheet
+        video_class --- ExcelData which contains all the information contained in the sheet.
+
+        Returns:
+        .txt files
+        """
         file_name = "Exported {0} {1}".format(excel_name, sheet_name)
         self.makedir(file_name)
 
@@ -456,6 +556,17 @@ class FileNameError(Exception):
 
 
 def readFile():
+    """
+    Prompt for the name of the excel file.
+    Read in and open the excel file.
+
+    Arguments:
+    None
+
+    Returns:
+    name of the excel file --- str
+    workbook ---openpyxl
+    """
     while True:
         try:
             excel_name = input(
@@ -472,6 +583,18 @@ def readFile():
 
 
 def readSheet(excel_file):
+    """
+    Prompt for the name of sheet
+    Read in and open a particular sheet in the excel file
+    
+    Arguments: 
+    excel_file ---openpyxl workbook
+
+    Return:
+    name of the sheet --- str
+    sheet --- openpyxl
+
+    """
     while True:
         try:
             sheet_name = input("Sheet name: ")
@@ -481,7 +604,7 @@ def readSheet(excel_file):
             print("Sheet '{0}' does not exist. \n".format(sheet_name))
 
 
-# read in Excel file and sheet name
+
 if __name__ == "__main__":
     video = ExcelData()
     topic = TopicData()
